8f52a08fe1da5d70db5d1be5a0247515e8923385
Khosefin <arianshoughi@gmail.com>
2024-08-02 10:40:10 +0330

    main page layout edited

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
 import Header from "@/app/_components/header";
+import Hero from "@/app/_components/Hero";
+import Course from "@/app/_components/Courses";
+import Footer from "@/app/_components/Footer";
+import KarSection from "@/app/_components/KarSection";
+import VideosSection from "@/app/_components/Videos";
 
 export default function Home() {
   return (
-      <main className="min-h-screen w-dvw relative">
+      <main className="min-h-screen relative">
           <Header/>
+          <div className='w-11/12 mx-auto'>
+              <Hero/>
+              <Course/>
+              <KarSection/>
+              <VideosSection/>
+          </div>
+          <Footer/>
       </main>
   );
 }
